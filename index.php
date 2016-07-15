8b4cdb18d42ce60a92836e930476a0d017ff60f2
Rick Smoke <rsmoke@umich.edu>
2016-07-15 14:15:17 -0400

    Fix verbage on form

--- index.php
+++ index.php
@@ -435,7 +435,7 @@ SQL;
           </div>
         </div>
         <div class="form-group required">
-          <label for="value">Approximate Value <em class="text-primary">(to the nearest dollar amount)</em></label>
+          <label for="value">Approximate Value <em class="text-primary">(to the nearest dollar amount if known or 0)</em></label>
           <div class="input-group">
           <span class="input-group-addon">$</span>
           <input type="text" class="form-control bfh-phone" required tabindex="170" id="value" name="value" value="<?php echo isset($value) ? $value : null;  ?>" data-format="dddddddd" />
