88f80003c92500de3640829362bec51b36c34fb5
rsmokeUM <rsmoke@umich.edu>
2021-01-20 15:00:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-  require_once($_SERVER["DOCUMENT_ROOT"] . "/../Support/configArtSurvey.php");
-  require_once($_SERVER["DOCUMENT_ROOT"] . "/../Support/basicLib.php");
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/artSurveyConfig.php");
+  require_once($_SERVER["DOCUMENT_ROOT"] . "/basicLib.php");
 
     $_SESSION['message'] = "<h4>&nbsp;</h4>";
 
